24487c641658d4adae299ff1bf4b57cbbb8c3e78
rachit-saurabh <rachit.saurabh123@gmail.com>
2026-04-29 13:39:32 +0530

    committing retryListener class

--- src/test/java/com/automation/listeners/RetryListener.java
+++ src/test/java/com/automation/listeners/RetryListener.java
@@ -8,10 +8,10 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 
 public class RetryListener implements IAnnotationTransformer {
-    
+
     @Override
-    public void transform(ITestAnnotation annotation, Class testClass, 
-        Constructor testConstructor, Method testMethod) {
+    public void transform(ITestAnnotation annotation, Class testClass,
+            Constructor testConstructor, Method testMethod) {
         if (testMethod != null) {
             annotation.setRetryAnalyzer(RetryAnalyzer.class);
         }
